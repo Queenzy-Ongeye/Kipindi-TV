86a0d3f73b13d6595e76a524003911596b56df3e
Queen-02 <queenzyblack176@gmail.com>
2022-07-26 09:34:47 +0300

    created home, favorite and latest fragments

--- app/src/main/java/dev/queen/kipindiauthentication/ui/HomeActivity.kt
+++ app/src/main/java/dev/queen/kipindiauthentication/ui/HomeActivity.kt
@@ -4,10 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
 import dev.queen.kipindiauthentication.R
+import dev.queen.kipindiauthentication.databinding.ActivityHomeBinding
+import dev.queen.kipindiauthentication.fragments.FavsFragment
+import dev.queen.kipindiauthentication.fragments.HomeFragment
+import dev.queen.kipindiauthentication.fragments.LatestFragment
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var bindingHome: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        bindingHome = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(bindingHome.root)
+    }
+
+    fun setUpBtmNav() {
+        bindingHome.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeMenu -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, HomeFragment())
+                        .commit()
+                    true
+                }
+
+                R.id.favs -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fcvHome, FavsFragment())
+                        .commit()
+                    true
+                }
+
+                R.id.latest -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fcvHome, LatestFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
